726dd67f46d7fa2d259e380b6114858ecb8d1149
c0d4ry <33789941+c0d4ry@users.noreply.github.com>
2020-01-18 22:09:43 +0100

    2020.01.18

---
+++ shop/resources/views/partials/header.blade.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Tintapatronok</a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+    <ul class="navbar-nav mr-auto">
+
+      <form class="form-inline ml-5 my-lg-0">
+        <input class="form-control mr-2" type="search" placeholder="Mit keres?" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Saját fiók
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Bejelentkezés</a>
+          <a class="dropdown-item" href="#">Regisztráció</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Kijelentkezés</a>
+        </div>
+      </li>
+
+
+
+    </ul>
+
+
+
+  </div>
+</nav>
