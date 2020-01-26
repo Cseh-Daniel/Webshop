8fbe43471989a9a04618772e84d1f27e258f0534
c0d4ry <33789941+c0d4ry@users.noreply.github.com>
2020-01-26 20:50:33 +0100

    php artisan make:model termekek -m

---
+++ shop/database/migrations/2020_01_26_194853_create_termekeks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTermekeksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('termekeks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('termekeks');
+    }
+}
